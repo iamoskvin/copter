c5252b660a99b44f6e87282012e599822f2e8764
iamoskvin <ilya@acrylstore.ru>
2021-06-04 10:30:03 +0300

    remove build and devel folders from git

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ilya/copter/code/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ilya/copter/code/catkin_ws/devel;/opt/ros/kinetic'.split(';') if '/home/ilya/copter/code/catkin_ws/devel;/opt/ros/kinetic' != '' else []
